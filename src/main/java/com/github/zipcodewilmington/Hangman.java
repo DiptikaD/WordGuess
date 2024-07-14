28735bfad65ad3cbd53edb064a4b83a434e9ef29
Devi <diptikadevi@gmail.com>
2024-07-14 13:37:03 -0400

    finished

--- src/main/java/com/github/zipcodewilmington/Hangman.java
+++ src/main/java/com/github/zipcodewilmington/Hangman.java
@@ -18,16 +18,13 @@ public class Hangman {
         int amountOfTries = 11;
         String[] listOfWords = {"bear", "hare", "lion", "rats"};
         String theWord = listOfWords[chooseRandomWord()];
-
-
-//        Scanner scanner = new Scanner (System.in);
-//        System.out.println("Let's Play HangMan v0.01");
-//        System.out.println("Current Guesses: "+amountOfTries);
-//        System.out.println("Enter a single character");
-//        String character = scanner.nextLine();
-//
+        String guess = "____";
 
         while (amountOfTries >= 1){
+            if (guess.equals(theWord)){
+                System.out.println("you win sweaty");
+                break;
+            }
             if (amountOfTries == 11){
                 System.out.println("Let's play hangman v0.01");
                 amountOfTries--;
@@ -36,11 +33,20 @@ public class Hangman {
                 Scanner scanner = new Scanner(System.in);
                 System.out.println("You have " + amountOfTries + "tries remaining.");
                 System.out.println("Enter a single character:");
-                char character = scanner.next().charAt(0);
-                isMatchOrNah(theWord, character);
-                System.out.println(isMatchOrNah(theWord,character));
-                boolean wasItTrue = isMatchOrNah(theWord,character);
-                checkWhereValueIsTrue(wasItTrue);
+                char characterGuess = scanner.next().charAt(0);
+                if(isMatchOrNah(theWord, characterGuess)) {
+                    int indexOfCorrectGuess = checkWhereValueIsTrue(theWord, characterGuess);
+                    System.out.println("You got a correct guess!");
+                    // _ _ a _
+                    guess = toStringPrintOut(guess, theWord, indexOfCorrectGuess);
+                    System.out.println(guess);
+                    amountOfTries--;
+                } else {
+                    System.out.println("You got guessed wrong!");
+                    amountOfTries--;
+                }
+
+
 
 
             }
@@ -52,32 +58,46 @@ public class Hangman {
 //    }
 
     public static boolean isMatchOrNah(String theword, char character) {
-        char [] ch = theword.toCharArray();
+        char[] ch = theword.toCharArray();
         for (int i = 0; i < ch.length;i++){
-        if (character ==(ch[i])){
-            for (int i = 0; i < ch.length;++){
-                if (ch[i].charAt[i] == character;){
-//need to figure out how to compare the character at ch[index] with the character typed
-                }
+            if (character ==(ch[i])){
+                return true;
             }
         }
-        }return false;
+        return false;
     }
 
 
 
 
 
-    public static int checkWhereValueIsTrue(boolean wasItTrue){
-
-
-        } else return 0;
+    public static Integer checkWhereValueIsTrue(String theword, char character){
+        char[] ch = theword.toCharArray();
+        Integer index = null;
+        for (int i = 0; i < ch.length;i++){
+            if (character ==(ch[i])){
+                index = i;
+            }
+        }
+        return index;
     }
 
 
     public static int chooseRandomWord(){
 
         Random random = new Random();
-        return random.nextInt(3);}
+        return random.nextInt(4);
+    }
+
+    public static String toStringPrintOut (String guess, String theWord, int indexOfCurrentGuess){
+        String[] splitGuesses = guess.split("");
+        splitGuesses[indexOfCurrentGuess] = Character.toString(theWord.charAt(indexOfCurrentGuess));
+
+        StringBuilder builder = new StringBuilder(splitGuesses.length);
+        for (String element : splitGuesses){
+            builder.append(element);
+        }
+        return builder.toString();
+    }
 
 }
